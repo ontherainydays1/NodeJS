f925e41a4a79c04d7f8146530141b896884b2231
bqt1703in <kingbqt03@gmail.com>
2023-09-26 18:11:48 +0700

    router

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
+app.use(morgan('combined'));
+app.engine("hbs",handlebars.engine({extname: ".hbs",}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+// route init
+route(app);
+
+app.listen(port, () => {
+  // console.log(`Example app listening on port ${port}`);
+});
